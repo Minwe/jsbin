da1f739d21dc881164fd46e62c98891c20543b0f
minwe <minwe@yunshipei.com>
2015-06-01 18:27:46 +0800

    update to 3.27.1
    # Conflicts:
    #	public/js/editors/libraries.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,8 @@
 
 var gulp = require('gulp');
 var upYunDest = require('gulp-upyun').upyunDest;
-console.log(process.env.upYunJSbin);
 
 gulp.task('default', function() {
-
   gulp.src('public/**/*')
       .pipe(upYunDest(process.env.upYunJSbin, {
         username: process.env.upYunUser,

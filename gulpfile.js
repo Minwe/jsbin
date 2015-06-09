5a23e2059bf3c7cf1a2a43453ef65c42a5f77c58
minwe <minwe@yunshipei.com>
2015-06-09 14:48:37 +0800

    using qn

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 'use strict';
 
 var gulp = require('gulp');
-var upYunDest = require('gulp-upyun').upyunDest;
+var qnUpload = require('gulp-qndn').upload;
 
 gulp.task('default', function() {
-  gulp.src('public/**/*')
-      .pipe(upYunDest(process.env.upYunJSbin, {
-        username: process.env.upYunUser,
-        password: process.env.upYunPwd,
-        verbose: true,
-        mkdir: true
+  return gulp.src('public/**/*')
+      .pipe(qnUpload({
+        prefix: 'jsbin',
+        qn: {
+          accessKey: process.env.qnAK,
+          secretKey: process.env.qnSK,
+          bucket: process.env.qnBucketUIS,
+          domain: process.env.qnDomainUIS
+        }
       }));
 });
